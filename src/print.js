6b282075daf6675ad428e0fd0ce180a3e91ceaea
Bruno8310 <gongl3730@gmail.com>
2020-08-27 19:24:47 +0800

    [homework][Bruno]feature: implement printTest and inline console.log

--- src/print.js
+++ src/print.js
@@ -1,22 +1,12 @@
 function printOwing (invoice) {
+  console.log('***********************\n' + '**** Customer Owes ****\n' + '***********************');
   let outstanding = 0;
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
-
-  // calculate outstanding
   for (const o of invoice.borderSpacing) {
     outstanding += o.amount;
   }
 
-  // record due date
-  const today = new Date();
-  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-
-  // print details
-  console.log(`name: ${invoice.customer}`);
-  console.log(`amount: ${outstanding}`);
-  console.log(`amount: ${invoice.dueDate.toLocaleDateString()}`);
+  invoice.dueDate = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 30);
+  console.log(`name: ${invoice.customer}\namount: ${outstanding}\ndate: ${invoice.dueDate.toLocaleDateString()}`);
 }
 
 module.exports = {
--- test/printTest.js
+++ test/printTest.js
@@ -3,6 +3,23 @@ const {
   printOwing
 } = require('../src/print');
 
-printTest('foo', t => {
-  t.pass();
-});
+printTest('printOwing case 1. Bruno calculate outstanding in today', t => {
+
+  const invoice = {
+    "dueDate": new Date(),
+    "customer": "Bruno",
+    "borderSpacing": [{
+        "amount": 1
+      },
+      {
+        "amount": 2
+      },
+      {
+        "amount": 3
+      }
+    ]
+
+  };
+  printOwing(invoice);
+  t.pass(); 
+})
