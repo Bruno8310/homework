f155331a3f4e6b656b8e286959203457dc7d7b4b
Bruno8310 <gongl3730@gmail.com>
2020-08-27 22:23:04 +0800

    [homework][Bruno]feature: implement Employee test case

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -3,6 +3,31 @@ const {
   Employee
 } = require('../src/employee');
 
-employeeTest('foo', t => {
-  t.pass();
+employeeTest('Employee case 1. given name Bruno type engineer when use tostring then return Bruno(enginner)', t => {
+  // given
+  let name = 'Bruno';
+  let type = 'engineer';
+
+  // when
+  let result = new Employee(name, type);
+
+  // then
+  t.is(result.toString(), 'Bruno (engineer)')
+
+});
+
+employeeTest('Employee case 2. given name Bruno type people when use tostring then return throw error', t => {
+  // given
+  let name = 'Bruno';
+  let type = 'people';
+
+  // when
+  try {
+    new Employee(name, type);
+    t.fail();
+  } catch (error) {
+    t.is(error.message, "Employee cannot be of type people");
+  }
+
+
 });
