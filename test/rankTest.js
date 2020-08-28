bc425546be1b310b0320f511df13210750398c27
Bruno8310 <gongl3730@gmail.com>
2020-08-28 11:47:00 +0800

    [homework][Bruno]feature: add rank test case 6

--- test/rankTest.js
+++ test/rankTest.js
@@ -100,10 +100,10 @@ rankTest('rank case 4. given history has china when use haschina method then ret
   // when
   let result = hasChina(history);
   // then
-  t.is(result, true); 
+  t.is(result, true);
 });
 
-rankTest('rank case 5. captainHistoryRisk. given voyage and history when use captainHistoryRisk method then return 4', t => {
+rankTest('rank case 5. given voyage and history when use captainHistoryRisk method then return 4', t => {
   // given
   const voyage = {
     zone: 'china',
@@ -130,3 +130,27 @@ rankTest('rank case 5. captainHistoryRisk. given voyage and history when use cap
   t.is(result, 4);
 });
 
+rankTest('rank case 6. given voyage and history when use voyageProfitFactor method then return 2', t => {
+  // given
+  const voyage = {
+    zone: 'west-africa',
+    length: 16,
+  };
+  const history = [{
+    zone: 'east-indies',
+    profit: 5,
+  }, {
+    zone: 'west-indies',
+    profit: 15,
+  },
+  {
+    zone: 'west-africa',
+    profit: 7,
+  },
+];
+  // when
+  let result = voyageProfitFactor(voyage, history);
+
+  // then
+  t.is(result, 2)
+})
